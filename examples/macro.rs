cfeee0b60782c726b5f9280917ca117339a7b051
Techcable <git@techcable.net>
2025-09-11 11:51:34 -0700

    Test macro working in impl block
    Runs fine

--- examples/macro.rs
+++ examples/macro.rs
@@ -2,7 +2,7 @@ use nounwind::nounwind;
 
 pub fn main() {
     x(String::from("Foo bar baz").chars());
-    failure();
+    Foo::failure();
 }
 
 #[nounwind]
@@ -10,7 +10,11 @@ pub fn x<T: IntoIterator<Item = char>>(y: T) -> u32 {
     y.into_iter().map(u32::from).sum()
 }
 
-#[nounwind]
-pub fn failure() -> String {
-    panic!("failure (will trigger abort)")
+struct Foo;
+
+impl Foo {
+    #[nounwind]
+    pub fn failure() -> String {
+        panic!("failure (will trigger abort)")
+    }
 }
